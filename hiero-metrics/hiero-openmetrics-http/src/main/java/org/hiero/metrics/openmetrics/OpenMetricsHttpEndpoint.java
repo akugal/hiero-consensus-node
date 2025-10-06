40b1c446cf3fe085e70f41bb8bb01249b841726b
Artur Kugal <artur.kugal@swirldslabs.com>
2025-10-06 17:50:02 -0400

    Add gzip
    Signed-off-by: Artur Kugal <artur.kugal@swirldslabs.com>

--- hiero-metrics/hiero-openmetrics-http/src/main/java/org/hiero/metrics/openmetrics/OpenMetricsHttpEndpoint.java
+++ hiero-metrics/hiero-openmetrics-http/src/main/java/org/hiero/metrics/openmetrics/OpenMetricsHttpEndpoint.java
@@ -5,17 +5,20 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.spi.HttpServerProvider;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.zip.GZIPOutputStream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hiero.metrics.api.export.MetricsSnapshot;
+import org.hiero.metrics.api.export.snapshot.MetricsSnapshot;
 import org.hiero.metrics.api.export.extension.PullingMetricsExporterAdapter;
 import org.hiero.metrics.api.export.extension.writer.OpenMetricsSnapshotsWriter;
+import org.hiero.metrics.api.export.extension.writer.UnsynchronizedByteArrayOutputStream;
 import org.hiero.metrics.openmetrics.config.OpenMetricsHttpEndpointConfig;
 
 public class OpenMetricsHttpEndpoint extends PullingMetricsExporterAdapter {
@@ -25,7 +28,9 @@ public class OpenMetricsHttpEndpoint extends PullingMetricsExporterAdapter {
     public static final String CONTENT_TYPE = "application/openmetrics-text; version=1.0.0; charset=utf-8";
 
     private final HttpServer server;
-    private final AtomicInteger lastResponseSize = new AtomicInteger(4096);
+    private final UnsynchronizedByteArrayOutputStream responseBuffer = new UnsynchronizedByteArrayOutputStream(1024);
+    private final ReentrantLock lock = new ReentrantLock();
+
     private final OpenMetricsSnapshotsWriter writer = OpenMetricsSnapshotsWriter.DEFAULT;
 
     public OpenMetricsHttpEndpoint(@NonNull OpenMetricsHttpEndpointConfig config) throws IOException {
@@ -42,6 +47,13 @@ public class OpenMetricsHttpEndpoint extends PullingMetricsExporterAdapter {
     }
 
     private void handleSnapshots(HttpExchange exchange) throws IOException {
+        if (!lock.tryLock()) {
+            logger.warn("Another request is being processed, rejecting this one");
+            exchange.sendResponseHeaders(503, 0); // Service Unavailable
+            exchange.close();
+            return;
+        }
+
         try {
             Optional<MetricsSnapshot> optionalSnapshot = getSnapshot();
             if (optionalSnapshot.isEmpty()) {
@@ -49,28 +61,51 @@ public class OpenMetricsHttpEndpoint extends PullingMetricsExporterAdapter {
                 return;
             }
 
-            ByteArrayOutputStream responseBuffer = new ByteArrayOutputStream(lastResponseSize.get() + 1024);
+            responseBuffer.reset();
             writer.write(optionalSnapshot.get(), responseBuffer);
-            lastResponseSize.set(responseBuffer.size());
-            logger.debug("Exporting metrics snapshot, sizeBytes={}", lastResponseSize.get());
-
-            exchange.getResponseHeaders().set("Content-Type", CONTENT_TYPE);
-            // TODO see if gzip is supported
 
             int contentLength = responseBuffer.size();
-            if (contentLength > 0) {
-                exchange.getResponseHeaders().set("Content-Length", String.valueOf(contentLength));
+            logger.debug("Exporting metrics snapshot, sizeBytes={}", contentLength);
+
+            exchange.getResponseHeaders().set("Content-Type", CONTENT_TYPE);
+            boolean useCompression = shouldUseCompression(exchange);
+            if (useCompression) {
+                exchange.getResponseHeaders().set("Content-Encoding", "gzip");
             }
 
-            exchange.sendResponseHeaders(200, contentLength);
-            responseBuffer.writeTo(exchange.getResponseBody());
+            if (useCompression) {
+                exchange.sendResponseHeaders(200, 0);
+                try (GZIPOutputStream gzipOutputStream = new GZIPOutputStream(exchange.getResponseBody())) {
+                    responseBuffer.writeTo(gzipOutputStream);
+                }
+            } else {
+                exchange.sendResponseHeaders(200, contentLength);
+                responseBuffer.writeTo(exchange.getResponseBody());
+            }
         } catch (RuntimeException e) {
-            // TODO error handling
+            //  TODO additional error handling ?
             logger.error("Error exporting metrics snapshot", e);
             exchange.sendResponseHeaders(500, 0);
         } finally {
             exchange.close();
+            lock.unlock();
         }
+    }
+
+    private boolean shouldUseCompression(HttpExchange exchange) {
+        List<String> encodingHeaders = exchange.getRequestHeaders().get("Accept-Encoding");
+        if (encodingHeaders == null) {
+            return false;
+        }
+        for (String encodingHeader : encodingHeaders) {
+            String[] encodings = encodingHeader.split(",");
+            for (String encoding : encodings) {
+                if (encoding.trim().equalsIgnoreCase("gzip")) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
